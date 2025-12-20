f16f76ac8794660ac68651ac279a4c46d257150a
Wisdom <xionxul12@gmail.com>
2025-12-20 15:14:43 +0100

    Add phone number formatter program - Reads phone number in format (xx) xxx-xxxx - Displays as xxx.xxx.xxxx with the leading zeros preserved - uses scanf with format specifiers for parsing

--- project2/phone-number.c
+++ project2/phone-number.c
@@ -2,5 +2,14 @@
 
 int main(void)
 {
-    
+    // Init Variables
+    int area_code, exchange, line_number;
+
+    printf("Enter phone number [(xxx) xxx-xxxx]: ");
+    scanf("(%3d) %3d-%4d", &area_code, &exchange, &line_number);
+
+    printf("You entered %03d.%03d.%04d", area_code, exchange, line_number);
+
+    return 0;
+
 }
